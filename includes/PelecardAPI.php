271bd4d02da52daa265cae3a571d1c5aa1495af0
Gregory Shilin <gshilin@gmail.com>
2017-08-27 14:18:06 +0300

    more data to store for priority

--- includes/PelecardAPI.php
+++ includes/PelecardAPI.php
@@ -1,196 +1,213 @@
 <?php
 
-class PelecardAPI {
-  /******  Array of request data ******/
-  var $vars_pay = array();
+class PelecardAPI
+{
+    /******  Array of request data ******/
+    var $vars_pay = array();
 
-  /******  Set parameter ******/
-  function setParameter($key, $value) {
-    $this->vars_pay[$key] = $value;
-  }
-
-  /******  Get parameter ******/
-  function getParameter($key) {
-    if (isset($this->vars_pay[$key])) {
-      return $this->vars_pay[$key];
-    } else {
-      return NULL;
-    }
-  }
-
-  /****** Request URL from PeleCard ******/
-  function getRedirectUrl() {
-    // Push constant parameters
-    $this->setParameter("ActionType", 'J4');
-    $this->setParameter("CardHolderName", 'hide');
-    $this->setParameter("CustomerIdField", 'hide');
-    $this->setParameter("Cvv2Field", 'must');
-    $this->setParameter("EmailField", 'hide');
-    $this->setParameter("TelField", 'hide');
-    $this->setParameter("FeedbackDataTransferMethod", 'POST');
-    $this->setParameter("FirstPayment", 'auto');
-    $this->setParameter("ShopNo", 1000); // TODO: What should be shop number?
-    $this->setParameter("SetFocus", 'CC');
-    $this->setParameter("HiddenPelecardLogo", true);
-    $cards = [
-      "Amex" => true,
-      "Diners" => false,
-      "Isra" => true,
-      "Master" => true,
-      "Visa" => true,
-    ];
-    $this->setParameter("SupportedCards", $cards);
-
-    $json = $this->arrayToJson();
-    $this->connect($json, '/init');
-
-    $error = $this->getParameter('Error');
-    if (is_array($error)) {
-      if ($error['ErrCode'] > 0) {
-        return array($error['ErrCode'], $error['ErrMsg']);
-      } else {
-        return array(0, $this->getParameter('URL'));
-      }
-    }
-  }
-
-  function connect($params, $action) {
-    // TODO: Read from settings
-    $ch = curl_init('https://gateway20.pelecard.biz/PaymentGW' . $action);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER,
-      array('Content-Type: application/json; charset=UTF-8', 'Content-Length: ' . strlen($params)));
-    $result = curl_exec($ch);
-    if ($result == '0') {
-      $this->vars_pay = [
-        'Error' => array(-1, 'Error')
-      ];
-    } elseif ($result == '1') {
-      $this->vars_pay = [
-        'Identified' => array(0, 'Identified')
-      ];
-    } else {
-      $this->stringToArray($result);
-    }
-  }
-
-  /******  Convert Hash to JSON ******/
-  function arrayToJson() {
-    return json_encode($this->vars_pay); //(PHP 5 >= 5.2.0)
-  }
-
-  /******  Convert String to Hash ******/
-  function stringToArray($data) {
-    if (is_array($data)) {
-      $this->vars_pay = $data;
-    } else {
-      $this->vars_pay = json_decode($data, true); //(PHP 5 >= 5.2.0)
-    }
-  }
-
-  /****** Validate Response ******/
-  function validateResponse($processor, $data) {
-    $PelecardTransactionId = $data['PelecardTransactionId'];
-    $PelecardStatusCode = $data['PelecardStatusCode'];
-    $ConfirmationKey = $data['ConfirmationKey'];
-    $UserKey = $data['UserKey'];
-    $amount = $data['amount'];
-
-    $this->vars_pay = [];
-    $this->setParameter("user", $processor["user_name"]);
-    $this->setParameter("password", $processor["password"]);
-    $this->setParameter("terminal", $processor["signature"]);
-    $this->setParameter("TransactionId", $PelecardTransactionId);
-
-    $json = $this->arrayToJson();
-    $this->connect($json, '/GetTransaction');
-
-    $error = $this->getParameter('Error');
-    if (is_array($error) && $error['ErrCode'] > 0) {
-      CRM_Core_Error::debug_log_message("Error[{error}]: {message}", ["error" => $error['ErrCode'], "message" => $error['ErrMsg']]);
-      return false;
+    /******  Set parameter ******/
+    function setParameter($key, $value)
+    {
+        $this->vars_pay[$key] = $value;
     }
 
-    $data = $this->getParameter('ResultData');
-    $this->stringToArray($data);
-
-    $this->vars_pay = [];
-    $this->setParameter("ConfirmationKey", $ConfirmationKey);
-    $this->setParameter("UniqueKey", $UserKey);
-    $this->setParameter("TotalX100", $amount * 100);
-
-    $json = $this->arrayToJson();
-    $this->connect($json, '/ValidateByUniqueKey');
-
-    $error = $this->getParameter('Error');
-    if (is_array($error) && $error['ErrCode'] > 0) {
-      CRM_Core_Error::debug_log_message("Error[{error}]: {message}", ["error" => $error['ErrCode'], "message" => $error['ErrMsg']]);
-      return false;
+    /******  Get parameter ******/
+    function getParameter($key)
+    {
+        if (isset($this->vars_pay[$key])) {
+            return $this->vars_pay[$key];
+        } else {
+            return NULL;
+        }
     }
 
-    // Store all parameters in DB
-    $db = new InvoiceDb();
-    if (!$db) {
-      echo $db->lastErrorMsg();
-      return false;
+    /****** Request URL from PeleCard ******/
+    function getRedirectUrl()
+    {
+        // Push constant parameters
+        $this->setParameter("ActionType", 'J4');
+        $this->setParameter("CardHolderName", 'hide');
+        $this->setParameter("CustomerIdField", 'hide');
+        $this->setParameter("Cvv2Field", 'must');
+        $this->setParameter("EmailField", 'hide');
+        $this->setParameter("TelField", 'hide');
+        $this->setParameter("FeedbackDataTransferMethod", 'POST');
+        $this->setParameter("FirstPayment", 'auto');
+        $this->setParameter("ShopNo", 1000); // TODO: What should be shop number?
+        $this->setParameter("SetFocus", 'CC');
+        $this->setParameter("HiddenPelecardLogo", true);
+        $cards = [
+            "Amex" => true,
+            "Diners" => false,
+            "Isra" => true,
+            "Master" => true,
+            "Visa" => true,
+        ];
+        $this->setParameter("SupportedCards", $cards);
+
+        $json = $this->arrayToJson();
+        $this->connect($json, '/init');
+
+        $error = $this->getParameter('Error');
+        if (is_array($error)) {
+            if ($error['ErrCode'] > 0) {
+                return array($error['ErrCode'], $error['ErrMsg']);
+            } else {
+                return array(0, $this->getParameter('URL'));
+            }
+        }
     }
 
-    $insert = $db->prepare("UPDATE payments SET response = :response, success = 1 WHERE id = :id");
-    if (!$insert) {
-      echo $db->lastErrorMsg();
-      return false;
-    }
-    $insert->bindValue(':id', $UserKey);
-    $insert->bindValue(':response', implode(",", $data));
-    $result = $insert->execute();
-    if (!$result) {
-      echo $db->lastErrorMsg();
-      return false;
-    }
-    $db->close();
-    return true;
-  }
-
-  function storeParameters($params) {
-    $db = new InvoiceDb();
-    if (!$db) {
-      echo $db->lastErrorMsg();
-    }
-
-    $insert = $db->prepare("INSERT INTO payments (id, name, amount, currency, email, phone, address, event, participants, org, income, is46, installments, success) VALUES ( :id, :name, :amount, :currency, :email, :phone, :address, :event, :participants, :org, :income, :is46, :installments, :success)");
-    if (!$insert) {
-      echo $db->lastErrorMsg();
+    function connect($params, $action)
+    {
+        // TODO: Read from settings
+        $ch = curl_init('https://gateway20.pelecard.biz/PaymentGW' . $action);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array('Content-Type: application/json; charset=UTF-8', 'Content-Length: ' . strlen($params)));
+        $result = curl_exec($ch);
+        if ($result == '0') {
+            $this->vars_pay = [
+                'Error' => array(-1, 'Error')
+            ];
+        } elseif ($result == '1') {
+            $this->vars_pay = [
+                'Identified' => array(0, 'Identified')
+            ];
+        } else {
+            $this->stringToArray($result);
+        }
     }
 
-    $insert->bindParam(':id', $params['id']);
-    $insert->bindParam(':name', $params['name']);
-    $insert->bindParam(':amount', $params['amount']);
-    $insert->bindParam(':currency', $params['currency']);
-    $insert->bindParam(':email', $params['email']);
-    $insert->bindParam(':phone', $params['phone']);
-    $insert->bindParam(':address', $params['address']);
-    $insert->bindParam(':event', $params['event']);
-    $insert->bindParam(':participants', $params['participants']);
-    $insert->bindParam(':org', $params['org']);
-    $insert->bindParam(':income', $params['income']);
-    $insert->bindParam(':is46', $params['is46']);
-    $insert->bindParam(':installments', $params['installments']);
-    $insert->bindParam(':success', $params['success']);
-
-    $result = $insert->execute();
-    if (!$result) {
-      echo $db->lastErrorMsg();
+    /******  Convert Hash to JSON ******/
+    function arrayToJson()
+    {
+        return json_encode($this->vars_pay); //(PHP 5 >= 5.2.0)
     }
-    $db->close();
-  }
+
+    /******  Convert String to Hash ******/
+    function stringToArray($data)
+    {
+        if (is_array($data)) {
+            $this->vars_pay = $data;
+        } else {
+            $this->vars_pay = json_decode($data, true); //(PHP 5 >= 5.2.0)
+        }
+    }
+
+    /****** Validate Response ******/
+    function validateResponse($processor, $data)
+    {
+        $PelecardTransactionId = $data['PelecardTransactionId'];
+        $PelecardStatusCode = $data['PelecardStatusCode'];
+        $ConfirmationKey = $data['ConfirmationKey'];
+        $UserKey = $data['UserKey'];
+        $amount = $data['amount'];
+        $cardnum = $data['CreditCardNumber'];
+        $cardexp = $data['CreditCardExpDate'];
+        $firstpay = $data['FirstPaymentTotal'];
+
+        $this->vars_pay = [];
+        $this->setParameter("user", $processor["user_name"]);
+        $this->setParameter("password", $processor["password"]);
+        $this->setParameter("terminal", $processor["signature"]);
+        $this->setParameter("TransactionId", $PelecardTransactionId);
+
+        $json = $this->arrayToJson();
+        $this->connect($json, '/GetTransaction');
+
+        $error = $this->getParameter('Error');
+        if (is_array($error) && $error['ErrCode'] > 0) {
+            CRM_Core_Error::debug_log_message("Error[{error}]: {message}", ["error" => $error['ErrCode'], "message" => $error['ErrMsg']]);
+            return false;
+        }
+
+        $data = $this->getParameter('ResultData');
+        $this->stringToArray($data);
+
+        $this->vars_pay = [];
+        $this->setParameter("ConfirmationKey", $ConfirmationKey);
+        $this->setParameter("UniqueKey", $UserKey);
+        $this->setParameter("TotalX100", $amount * 100);
+
+        $json = $this->arrayToJson();
+        $this->connect($json, '/ValidateByUniqueKey');
+
+        $error = $this->getParameter('Error');
+        if (is_array($error) && $error['ErrCode'] > 0) {
+            CRM_Core_Error::debug_log_message("Error[{error}]: {message}", ["error" => $error['ErrCode'], "message" => $error['ErrMsg']]);
+            return false;
+        }
+
+        // Store all parameters in DB
+        $db = new InvoiceDb();
+        if (!$db) {
+            echo $db->lastErrorMsg();
+            return false;
+        }
+
+        $insert = $db->prepare("UPDATE payments SET response = :response, cardnum = :cardnum, cardexp = :cardexp, firstpay = :firstpay, success = 1 WHERE id = :id");
+        if (!$insert) {
+            echo $db->lastErrorMsg();
+            return false;
+        }
+        $insert->bindValue(':id', $UserKey);
+        $insert->bindValue(':cardnum', $cardnum);
+        $insert->bindValue(':cardexp', $cardexp);
+        $insert->bindValue(':firstpay', $firstpay);
+        $insert->bindValue(':response', implode(",", $data));
+        $result = $insert->execute();
+        if (!$result) {
+            echo $db->lastErrorMsg();
+            return false;
+        }
+        $db->close();
+        return true;
+    }
+
+    function storeParameters($params)
+    {
+        $db = new InvoiceDb();
+        if (!$db) {
+            echo $db->lastErrorMsg();
+        }
+
+        $insert = $db->prepare("INSERT INTO payments (id, name, amount, currency, email, phone, address, event, participants, org, income, is46, installments, success) VALUES ( :id, :name, :amount, :currency, :email, :phone, :address, :event, :participants, :org, :income, :is46, :installments, :success)");
+        if (!$insert) {
+            echo $db->lastErrorMsg();
+        }
+
+        $insert->bindParam(':id', $params['id']);
+        $insert->bindParam(':name', $params['name']);
+        $insert->bindParam(':amount', $params['amount']);
+        $insert->bindParam(':currency', $params['currency']);
+        $insert->bindParam(':email', $params['email']);
+        $insert->bindParam(':phone', $params['phone']);
+        $insert->bindParam(':address', $params['address']);
+        $insert->bindParam(':event', $params['event']);
+        $insert->bindParam(':participants', $params['participants']);
+        $insert->bindParam(':org', $params['org']);
+        $insert->bindParam(':income', $params['income']);
+        $insert->bindParam(':is46', $params['is46']);
+        $insert->bindParam(':installments', $params['installments']);
+        $insert->bindParam(':success', $params['success']);
+
+        $result = $insert->execute();
+        if (!$result) {
+            echo $db->lastErrorMsg();
+        }
+        $db->close();
+    }
 }
 
-class InvoiceDb extends SQLite3 {
-  function __construct() {
-    $this->open("db/bb2prio.db", SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
-    $sql = <<<EOF
+class InvoiceDb extends SQLite3
+{
+    function __construct()
+    {
+        $this->open("db/bb2prio.db", SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
+        $sql = <<<EOF
           CREATE TABLE IF NOT EXISTS payments (
             id            CHAR(50)  PRIMARY KEY NOT NULL,
             name          TEXT      NOT NULL,
@@ -204,27 +221,33 @@ class InvoiceDb extends SQLite3 {
             org           CHAR(50)  NOT NULL,
             income        CHAR(20)  NOT NULL,
             is46          INTEGER   NOT NULL,
+            cardnum       CHAR(16)  DEFAULT '',
+            cardexp       CHAR(5)   DEFAULT '',
+            firstpay      REAL      DEFAULT '',
             installments  INT       NOT NULL,
             response      TEXT,
-            success       INTEGER DEFAULT 0,   
-            reported2prio INTEGER DEFAULT 0
+            success       INTEGER   DEFAULT 0,   
+            reported2prio INTEGER   DEFAULT 0
           );
 EOF;
 
-    $this->exec($sql);
-  }
+        $this->exec($sql);
+    }
 }
 
 /******  Base64 Functions  ******/
-function base64_url_encode($input) {
-  return strtr(base64_encode($input), '+/', '-_');
+function base64_url_encode($input)
+{
+    return strtr(base64_encode($input), '+/', '-_');
 }
 
-function encodeBase64($data) {
-  $data = base64_encode($data);
-  return $data;
+function encodeBase64($data)
+{
+    $data = base64_encode($data);
+    return $data;
 }
 
-function base64_url_decode($input) {
-  return base64_decode(strtr($input, '-_', '+/'));
+function base64_url_decode($input)
+{
+    return base64_decode(strtr($input, '-_', '+/'));
 }
