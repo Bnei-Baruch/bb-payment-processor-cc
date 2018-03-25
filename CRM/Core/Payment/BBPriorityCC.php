f6b7a22267f839aa04ed51f72faa2f7dadaeadd8
Gregory Shilin <gshilin@gmail.com>
2018-03-25 12:41:01 +0000

    fix search function

--- CRM/Core/Payment/BBPriorityCC.php
+++ CRM/Core/Payment/BBPriorityCC.php
@@ -498,8 +498,9 @@ class CRM_Core_Payment_BBPriorityCC extends CRM_Core_Payment
     {
         $found = [];
         array_walk_recursive($haystack, function ($value, $key) use (&$found, $needle) {
-            if ($key == $needle)
+            if (gettype($key) == 'string' && $key == $needle) {
                 $found[] = $value;
+	    }
         });
         return $found[0];
     }
