4e82b86b0dd3ae453e7aa610ca947caec919505a
Gregory Shilin <gshilin@gmail.com>
2018-10-02 15:02:05 +0300

    Support Credit transactions

--- includes/PelecardAPI.php
+++ includes/PelecardAPI.php
@@ -159,6 +159,9 @@ class PelecardAPI
         $cardnum = $data['CreditCardNumber'] . '';
         $cardexp = $data['CreditCardExpDate'] . '';
         $amount = $data['DebitTotal'] / 100.00;
+        if ($data['DebitType'] == '51') {
+            $amount = -$amount;
+        }
         $installments = $data['TotalPayments'];
         if ($installments == 1) {
             $firstpay = $amount;
