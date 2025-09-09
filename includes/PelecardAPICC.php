355613795ff77e7c7237b1e0c8810592b95e2f76
Gregory Shilin <gshilin@gmail.com>
2025-09-09 03:28:27 +0300

    Added updated_at to civicrm_bb_payment_responses

--- includes/PelecardAPICC.php
+++ includes/PelecardAPICC.php
@@ -195,7 +195,7 @@ class PelecardAPICC {
             12 => array($updatedat, 'String'),
         );
         CRM_Core_DAO::executeQuery(
-            'INSERT INTO civicrm_bb_payment_responses(trxn_id, cid, cardtype, cardnum, cardexp, firstpay, installments, response, amount, token, is_regular, approval, created_at) 
+            'INSERT INTO civicrm_bb_payment_responses(trxn_id, cid, cardtype, cardnum, cardexp, firstpay, installments, response, amount, token, is_regular, approval, created_at, updated_at) 
                    VALUES (%1, %2, %3, %4, %5, %6, %7, %8, %9, %10, 1, %11, NOW(), %12)', $query_params);
 
         return [true, $data];
